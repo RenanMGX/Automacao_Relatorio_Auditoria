6c8e4db59268f1c8d5c67a02b91308b826034612
Renan Brian Hermenegildo Oliveira <renan.oliveira@patrimar.com.br>
2024-12-16 10:13:27 -0300

    primeiro commit

---
+++ Entities/dependencies/default_config.py
@@ -0,0 +1,10 @@
+from typing import Dict
+from getpass import getuser
+
+default:Dict[str, Dict[str,object]] = {
+    'log': {
+        'hostname': 'Patrimar-RPA',
+        'port': '80',
+        'token': ''
+    }
+}
